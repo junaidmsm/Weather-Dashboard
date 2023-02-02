6cbaa341a446ea95ff039cc3ed4c1e8803f4ea59
BootCampTech <mjunaid@live.co.uk>
2023-02-02 16:33:39 +0000

    further changes to script.js

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -35,47 +35,48 @@ searchButton.submit(function (event)  {
         }).then(function (response) {
             // list-group append an li to it with just set text
              console.log(response.name);
-            var cityName = $(".list-group").addClass("list-group-item");
+            var cityName = $("<ul>").addClass("list-group");
             cityName.append("<li>" + response.name + "</li>");
             // Local storage
             var local = localStorage.setItem(keyCount, response.name);
             keyCount = keyCount + 1;
 
             // Start Current Weather append 
-            var currentCard = $(".currentCard").append("<div>").addClass("card-body");
-            currentCard.empty();
-            var currentName = currentCard.append("<p>");
+            var currentCard = $("<div>").addClass("card-body");
+         
+            var currentName = $("<p>");
             // .addClass("card-text");
-            currentCard.append(currentName);
-
+			
             // Adjust Date 
             var timeUTC = new Date(response.dt * 1000);
-            currentName.append(response.name + " " + timeUTC.toLocaleDateString("en-US"));
-            currentName.append(`<img src="https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png">`);
+            currentName.text(response.name + " " + timeUTC.toLocaleDateString("en-US"));
+            currentCard.append(currentName);
+            currentCard.append(`<img src="https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png">`);
             // Add Temp 
-            var currentTemp = currentName.append("<p>");
             // .addClass("card-text");
-            currentName.append(currentTemp);
-            currentTemp.append("<p>" + "Temperature: " + response.main.temp + "</p>");
+			
+            var currentTemp =$("<p>");
+            currentTemp.text( "Temperature: " + response.main.temp );
+			currentCard.append(currentTemp)
             // Add Humidity
-            currentTemp.append("<p>" + "Humidity: " + response.main.humidity + "%" + "</p>");
+            currentCard.append("<p>" + "Humidity: " + response.main.humidity + "%" + "</p>");
             // // Add Wind Speed: 
-            currentTemp.append("<p>" + "Wind Speed: " + response.wind.speed + "</p>");
-			$("#current").append(currentTemp)
+            currentCard.append("<p>" + "Wind Speed: " + response.wind.speed + "</p>");
             // UV Index URL
             var urlUV = `https://api.openweathermap.org/data/2.5/uvi?appid=603e7c817d7a1ac25b9f13f2bc21b35f&lat=${response.coord.lat}&lon=${response.coord.lon}`;
-
+			
             // UV Index
             $.ajax({
-                url: urlUV,
+				url: urlUV,
                 method: "GET"
             }).then(function (response) {
-
-                var currentUV = currentTemp.append("<p>" + "UV Index: " + response.value + "</p>").addClass("card-text");
+				
+				var currentUV = $("<p>" + "UV Index: " + response.value + "</p>").addClass("card-text");
                 currentUV.addClass("UV");
-                currentTemp.append(currentUV);
+                currentCard.append(currentUV)
                 // currentUV.append("UV Index: " + response.value);
             });
+			$("#current").html(currentCard)
 
         });
 
